a7aa15677dd2d88f26eb9fa7afe0ad61f2e899c6
sergey <s.glebko@arateg.com>
2020-06-01 20:46:52 +0300

    first commit

---
+++ precache-manifest.21227bd9d096113c46c569193f36f22d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296c24c615d6a79ae9a7b5322c043319",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfb6db5dc07f1086169e",
+    "url": "/static/css/main.980d672d.chunk.css"
+  },
+  {
+    "revision": "a4844855b36ff887be08",
+    "url": "/static/js/2.7424059e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7424059e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb6db5dc07f1086169e",
+    "url": "/static/js/main.cab91231.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  }
+]);
